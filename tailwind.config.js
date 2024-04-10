a1b68482470bab47f59bc599c381887e3aad8ff6
fschatbot <s.himanshu.sultania@fountainheadschools.org>
2024-04-10 09:41:25 -0700

    Made the page to a certain extent

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ["./src/**/*.{html,js}"],
-	theme: { extend: {} },
+	content: ["./**/*.{html,js}"],
+	theme: {
+		extend: {
+			colors: {
+				"container-bg": "#263A53",
+				"container-txt": "#FFFFFF",
+			},
+		},
+	},
 	plugins: [],
 };
