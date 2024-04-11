db6a61def351d221ba5945c3dc29d0f35fb03608
fschatbot <s.himanshu.sultania@fountainheadschools.org>
2024-04-11 10:06:23 -0700

    Completed the home page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,34 @@
 /** @type {import('tailwindcss').Config} */
+
+const flatten = (obj, sep = "-") =>
+	Object.assign(
+		{},
+		...(function _flatten(o, p = "") {
+			return [].concat(...Object.keys(o).map((k) => (typeof o[k] === "object" ? _flatten(o[k], k + sep) : { [p + k]: o[k] })));
+		})(obj)
+	);
+
 module.exports = {
-	content: ["./**/*.{html,js}"],
+	content: ["*.{html,js}"],
 	theme: {
 		extend: {
 			colors: {
-				"container-bg": "#263A53",
+				"container-bg": "#D9D9D9",
 				"container-txt": "#FFFFFF",
 			},
 		},
 	},
-	plugins: [],
+	plugins: [
+		function ({ matchUtilities, theme }) {
+			matchUtilities(
+				{
+					"bg-stripes": (value) => ({
+						backgroundImage: `linear-gradient(45deg, ${value} 12.50%, transparent 12.50%, transparent 50%, ${value} 50%, ${value} 62.50%, transparent 62.50%, transparent 100%)`,
+						backgroundSize: "5.66px 5.66px",
+					}),
+				},
+				{ values: flatten(theme("colors")) }
+			);
+		},
+	],
 };
